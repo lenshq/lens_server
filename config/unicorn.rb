63169cc21e1ee969e1d79ed0c28571386efe5cd3
Mikhail Tabunov <tmp.over@gmail.com>
2015-05-15 16:44:15 +0300

    Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 worker_processes 10
-working_directory "/home2/apps/lens/current"
+working_directory "/home2/apps/lens-server/current"
 listen '0.0.0.0:10050'
 timeout 60
-pid "/home2/apps/lens/tmp/pids/unicorn.pid"
+pid "/home2/apps/lens-server/tmp/pids/unicorn.pid"
 preload_app true
 
 GC.respond_to?(:copy_on_write_friendly=) and
