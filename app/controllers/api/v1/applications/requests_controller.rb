bcf83110ab44ab5b16af561a0954262ceec1e288
Andrew Kumanyaev <me@zzet.org>
2015-06-05 19:10:08 +0300

    fix

--- app/controllers/api/v1/applications/requests_controller.rb
+++ app/controllers/api/v1/applications/requests_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Applications::RequestController < Api::ApplicationController
+class Api::V1::Applications::RequestsController < Api::ApplicationController
   def index
     @requests = application.requests(params)
     @data = @requests.map do |req|
