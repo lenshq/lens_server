dc39f8a69143f06784a426cce515a5d6c7f2d060
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2015-06-05 20:30:15 +0300

    Dates

--- app/assets/javascripts/components/FilterForm/FilterForm.jsx
+++ app/assets/javascripts/components/FilterForm/FilterForm.jsx
@@ -11,9 +11,33 @@ var FilterForm = React.createClass({
       {id: 1, name: "App ID #1"}
     ];
 
+    var weekAgo = new Date()
+    weekAgo.setDate(weekAgo.getDate() - 7);
+
+    today = new Date(Date.now());
+    weekAgo = new Date(weekAgo);
+
+    today = [today.getFullYear(), today.getMonth(), today.getDay()];
+    weekAgo = [weekAgo.getFullYear(), weekAgo.getMonth(), weekAgo.getDay()];
+
+    var dates = [today, weekAgo];
+
+    for(var i in dates) {
+      dates[i] = dates[i].map(function(e){
+        console.log("e is", e);
+        e = e.toString()
+        if(e.length < 2) {
+          console.log("Remap")
+          e = "0" + e;
+        };
+
+        return e;
+      });
+    }
+
     return {
-      dateFrom: "1988-01-07",
-      dateTo :"2015-01-07",
+      dateFrom: dates[1].join("-"),
+      dateTo : dates[0].join("-"),
       urlFilter: "",
       appsIds: appsIds,
       appId: 1
