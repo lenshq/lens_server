62100b0b01471607e5b52748e18b9d54aaa79ea5
Andrew Kumanyaev <me@zzet.org>
2015-06-05 19:41:29 +0300

    показываем конктрентый запрос

--- app/controllers/api/v1/applications/requests_controller.rb
+++ app/controllers/api/v1/applications/requests_controller.rb
@@ -12,6 +12,19 @@ class Api::V1::Applications::RequestsController < Api::ApplicationController
     render json: @data
   end
 
+  def show
+    @request = application.requests(params)
+    @data = {
+      id: @request["id"],
+      url: @reques["url"],
+      datetime: @reques['datetime'],
+      duration: @reques['duration']
+    }
+    render json: @data
+  end
+
+  private
+
   def application
     @application ||= current_user.participate_applications.find(params[:application_id])
   end
