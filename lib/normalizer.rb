e9eff398835db51350fbbc44159a65df19d6bca1
Andrew Kumanyaev <me@zzet.org>
2015-07-26 17:30:20 +0300

    2 вараинта работы парсилки ответов

--- lib/normalizer.rb
+++ lib/normalizer.rb
@@ -1,15 +1,24 @@
 class Normalizer
-  def initialize(record)
+  def initialize(record, skip_initial_events = true)
     @record = record
+    @skip_initial_events = skip_initial_events
   end
 
   def normalize
+    return nil if @record[:etype].starts_with?('!') && @skip_initial_events
+
     klass.normalize(@record)
   end
 
   def klass
     @klass ||= begin
-                 (['normalizers'] + @record[:etype].split('.').reverse).join('/').camelize.constantize
+                 source_type = @record[:etype].dup
+
+                 unless @skip_initial_events
+                   source_type.gsub!('!', 'initial_')
+                 end
+
+                 (['normalizers'] + source_type.split('.').reverse).join('/').camelize.constantize
                rescue
                  Normalizers::Base
                end
