ffdd47210828046a0ba6912e54231e9a1bc70bf8
Andrew Kumanyaev <me@zzet.org>
2015-07-26 17:37:51 +0300

    Обрабатываем чтение из кеша

---
+++ lib/normalizers/action_controller/read_fragment.rb
@@ -0,0 +1,15 @@
+class Normalizers::ActionController
+  class ReadFragment < Normalizers::Base
+    class << self
+      def type
+        'app.cache.read.fragment'.freeze
+      end
+
+      def normalize(record)
+        {
+          content: record[:key]
+        }.merge(super)
+      end
+    end
+  end
+end
