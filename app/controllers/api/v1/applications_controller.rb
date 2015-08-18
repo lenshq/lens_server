e08a54f2d784bd7e6fa67fc44d6282d92099544e
Andrew Kumanyaev <me@zzet.org>
2015-08-18 16:37:45 +0400

    fix spec

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -1,24 +1,26 @@
-module Api::V1
-  class ApplicationsController < ApplicationController
-    def index
-      apps = Application.all
-      render json: apps, each_serializer: ApplicationSerializer
-    end
+module Api
+  module V1
+    class ApplicationsController < Api::V1::ApplicationController
+      def index
+        apps = Application.all
+        render json: apps, each_serializer: ApplicationSerializer
+      end
 
-    def create
-      app = Application.create application_params
-      render json: app
-    end
+      def create
+        app = Application.create application_params
+        render json: app
+      end
 
-    def show
-      app = Application.find params[:id]
-      render json: app
-    end
+      def show
+        app = Application.find params[:id]
+        render json: app
+      end
 
-    private
+      private
 
-    def application_params
-      params.require(:application).permit(:title, :description)
+      def application_params
+        params.require(:application).permit(:title, :description, :domain)
+      end
     end
   end
 end
