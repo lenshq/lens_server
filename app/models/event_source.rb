50633400258609fa5873d8a9f5b292bf1628eadc
Andrew Kumanyaev <me@zzet.org>
2015-08-19 02:45:50 +0400

    Event source calculations for source list on application page

--- app/models/event_source.rb
+++ app/models/event_source.rb
@@ -7,4 +7,20 @@ class EventSource < ActiveRecord::Base
   validates :source, presence: true
   validates :endpoint, presence: true
   validates :application_id, uniqueness: { scope: [:source, :endpoint] }
+
+  def sum_duration
+    pages.sum(:duration)
+  end
+
+  def avg_duration
+    pages.average(:duration)
+  end
+
+  def min_duration
+    pages.minimum(:duration)
+  end
+
+  def max_duration
+    pages.maximum(:duration)
+  end
 end
