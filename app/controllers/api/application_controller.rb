d2f48657f38971196d7841d2c6290773bb363e52
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-10-15 20:24:41 +0300

    restrict access to API #14

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,7 +1,12 @@
 module Api
   class ApplicationController < ::ActionController::API
+    before_filter :authenticate!
 
     protected
+
+    def authenticate!
+      render json: { error: 'Unauthorized' }, status: 401 unless api_token
+    end
 
     def api_token
       @api_token ||= params[:api_token] || request.headers['X-Auth-Token']
