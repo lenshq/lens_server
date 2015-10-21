14c235fa0f1a87b9938ba37a9c8da80bb7b37486
Andrew Kumanyaev <me@zzet.org>
2015-10-21 15:38:51 +0300

    Rename methods get_*

--- app/finders/event_source_finder.rb
+++ app/finders/event_source_finder.rb
@@ -8,19 +8,19 @@ class EventSourceFinder
 
   def get
     {
-      event_sources: get_sources,
-      requests: get_requests
+      event_sources: event_sources,
+      requests: requests
     }
   end
 
   private
 
-  def get_sources
+  def event_sources
     sources = @application.event_sources.includes(:requests)
     filter_sources(sources)
   end
 
-  def get_requests
+  def requests
     sql = "
     SELECT d.date, COUNT(requests.id) FROM (
       SELECT * FROM generate_series(date_trunc('#{@filter_options[:period]}', '#{@filter_options[:from_date].to_s(:db)}'::timestamp with time zone),date_trunc('#{@filter_options[:period]}', '#{@filter_options[:to_date].to_s(:db)}'::text::date::timestamp with time zone), '1 #{@filter_options[:period]}') date_trunc(date)
