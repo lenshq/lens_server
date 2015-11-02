089c123cfc3ff61d98b5bcd2a7e2874a4d7e2598
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-11-03 01:04:26 +0300

    refactor request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,74 +1,64 @@
 class Request
   include ActiveModel::Model
 
-  attr_accessor :application, :scenario, :event_source, :controller, :action,
-    :timestamp, :duration, :started_at, :finished_at, :raw_event
+  attr_accessor(
+    :application, :scenario, :event_source, :controller, :action, :timestamp, :duration,
+    :started_at, :finished_at, :raw_event
+  )
 
   class << self
     def sum_duration(event_source:, from:, to:)
-      from ||= Time.now.utc - 1.week
-      to ||= Time.now.utc
-
-      query = Druid::Query::Builder.new
-      query.double_sum(:duration).
-        interval(from, to).
-        granularity(:all).
-        filter(event_source: event_source.id)
-
-      result = get(query)
+      result = query_by_period(from, to) do |query|
+        query.double_sum(:duration).
+          granularity(:all).
+          filter(event_source: event_source.id)
+      end
 
       result.first['result']['duration']
     end
 
     def avg_duration(event_source:, from:, to:)
-      from ||= Time.now.utc - 1.week
-      to ||= Time.now.utc
-
-      query = Druid::Query::Builder.new
-
-      query.double_sum(:duration).count(:count).
-        interval(from, to).
-        granularity(:all).
-        filter(event_source: event_source.id).
-        postagg { (duration / count).as avg }
-
-      result = get(query)
+      result = query_by_period(from, to) do |query|
+        query.double_sum(:duration).count(:count).
+          granularity(:all).
+          filter(event_source: event_source.id).
+          postagg { (duration / count).as avg }
+      end
 
       result.first['result']['avg']
     end
 
     def count(event_source:, from:, to:)
-      from ||= Time.now.utc - 1.week
-      to ||= Time.now.utc
-
-      query = Druid::Query::Builder.new
-
-      query.count(:count).
-        interval(from, to).
-        granularity(:all).
-        filter(event_source: event_source.id)
-
-      result = get(query)
+      result = query_by_period(from, to) do |query|
+        query.count(:count).
+          granularity(:all).
+          filter(event_source: event_source.id)
+      end
 
       result.first['result']['count']
     end
 
     def by_application(application)
-      from = Time.now.utc - 1.week
-      to = Time.now.utc
+      result = query_by_period do |query|
+        query.count(:duration).
+          granularity(:minute).
+          group_by(:event_source).
+          filter(application: application.id)
+      end
 
-      query = Druid::Query::Builder.new
-
-      query.count(:duration).
-        granularity(:minute).
-        # group_by(:event_source).
-        interval(from, to).
-        filter(application: application.id)
-
-      get(query)
+      result
     end
 
     private
+
+    def query_by_period(from: nil, to: nil)
+      from ||= Time.now.utc - 1.week
+      to ||= Time.now.utc
+
+      query = Druid::Query::Builder.new
+      yield query.interval(from, to)
+      get(query)
+    end
 
     def datasource
       "broker/#{LensServer.config.druid.datasource.requests}"
