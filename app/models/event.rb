459147975d8cce6b3bb04e550cf9f1dface7854f
Alex Klyanchin <jetblackmeerkat@gmail.com>
2015-11-03 00:42:16 +0300

    remove draft code

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,22 +1,8 @@
 class Event
   include ActiveModel::Model
 
-  attr_accessor :application, :scenario, :controller, :action,
-    :timestamp, :event_type, :content, :duration, :position,
-    :started_at, :finished_at
-
-  class << self
-    def avg_duration
-      query = Druid::Query.new("#{LensServer.config.kafka.topic}/events").long_sum('duration')
-
-      ServiceLocator.druid_client.send(query)
-    end
-
-    def test
-      query = Druid::Query.new("#{LensServer.config.kafka.topic}/events").
-        group_by('duration')
-
-      ServiceLocator.druid_client.send(query)
-    end
-  end
+  attr_accessor(
+    :application, :scenario, :controller, :action, :timestamp, :event_type, :content, :duration,
+    :position, :started_at, :finished_at
+  )
 end
