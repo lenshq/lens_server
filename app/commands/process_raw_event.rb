ad9a672fa9cff3e7e795266087fc814269d423f9
Andrew Kumanyaev <me@zzet.org>
2015-11-08 00:58:32 +0300

    Refactoring of process raw event command with new raw_event interface

--- app/commands/process_raw_event.rb
+++ app/commands/process_raw_event.rb
@@ -17,29 +17,15 @@ class ProcessRawEvent
   private
 
   def process_raw_event(raw_event)
-    application = raw_event.application
-    parsed_raw_event = ParsedRawEvent.new(raw_event)
+    event_source = find_or_create_event_source(raw_event)
+    scenario = find_or_create_scenario(
+      event_source: event_source,
+      hash: generate_scenario_hash(raw_event.details)
+    )
+    raw_event.update(scenario: scenario)
 
-    meta = parsed_raw_event.meta
-    details = parsed_raw_event.details
-    scenario_hash = generate_scenario_hash(details)
-
-    event_source = find_or_create_event_source(application: application, meta: meta)
-    scenario = find_or_create_scenario(event_source: event_source, hash: scenario_hash)
-    raw_event.scenario = scenario
-    raw_event.save
-
-    request = Request.new(
-      request_hash(scenario: scenario, raw_event: raw_event, meta: meta)
-    ).to_json
-    Commands::SendToKafka.call request
-
-    events = details.each_with_index.map do |row, index|
-      Event.new(
-        event_hash(scenario: scenario, details: row, meta: meta, index: index)
-      ).to_json
-    end
-    Commands::SendToKafka.call events
+    store_request(raw_event)
+    store_events(raw_event)
   end
 
   def generate_scenario_hash(details)
@@ -47,12 +33,52 @@ class ProcessRawEvent
     Scenario.hash_from_string(scenario_key)
   end
 
-  def find_or_create_event_source(application:, meta:)
-    application.event_sources.find_or_create_by(source: meta[:controller], endpoint: meta[:action])
+  def find_or_create_event_source(raw_event)
+    application = raw_event.application
+
+    application.event_sources.find_or_create_by(
+      source: raw_event.meta[:controller],
+      endpoint: raw_event.meta[:action]
+    )
   end
 
   def find_or_create_scenario(event_source:, hash:)
     event_source.scenarios.find_or_create_by(events_hash: hash)
+  end
+
+  def store_request(raw_event)
+    request = Request.new(request_hash(raw_event)).to_json
+
+    Commands::SendToKafka.call request
+  end
+
+  def store_events(raw_event)
+    events = raw_event.details.each_with_index.map do |row, index|
+      Event.new(
+        event_hash(scenario: raw_event.scenario, details: row, meta: raw_event.meta, index: index)
+      ).to_json
+    end
+
+    Commands::SendToKafka.call events
+  end
+
+  def request_hash(raw_event)
+    scenario = raw_event.scenario
+    event_source = scenario.event_source
+    meta = raw_event.meta
+
+    {
+      timestamp: Time.at(meta[:start]).to_s(:iso8601),
+      scenario: scenario.events_hash,
+      event_source: event_source.id,
+      application: event_source.application.id,
+      controller: meta[:controller],
+      action: meta[:action],
+      duration: meta[:duration],
+      started_at: meta[:start],
+      finished_at: meta[:finish],
+      raw_event: raw_event.id
+    }
   end
 
   def base_hash(scenario:, meta:)
@@ -74,22 +100,5 @@ class ProcessRawEvent
       finished_at: details[:finish],
       position: index
     )
-  end
-
-  def request_hash(scenario:, raw_event:, meta:)
-    event_source = scenario.event_source
-
-    {
-      timestamp: Time.at(meta[:start]).to_s(:iso8601),
-      scenario: scenario.events_hash,
-      event_source: event_source.id,
-      application: event_source.application.id,
-      controller: meta[:controller],
-      action: meta[:action],
-      duration: meta[:duration],
-      started_at: meta[:start],
-      finished_at: meta[:finish],
-      raw_event: raw_event.id
-    }
   end
 end
