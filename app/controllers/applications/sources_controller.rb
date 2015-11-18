f6c4497338772bb2bd3372ea2b06afd58997cbc4
Andrew Kumanyaev <me@zzet.org>
2015-11-18 20:45:07 +0300

    Update sources page

--- app/controllers/applications/sources_controller.rb
+++ app/controllers/applications/sources_controller.rb
@@ -4,12 +4,15 @@ class Applications::SourcesController < Applications::ApplicationController
     @event_source = application.event_sources.find(params[:id])
 
     scenarios = in_period(@event_source).inject({}) do |acc, row|
-      acc[row['event']['scenario']] = row['event']['count']
+      acc[row['event']['scenario']] = {
+        count: row['event']['count'],
+        duration: row['event']['duration']
+      }
       acc
     end
     @scenarios = @event_source.scenarios.where(events_hash: scenarios.keys).map do |scenario|
       { scenario.id => scenarios[scenario.events_hash] }
-    end.sort_by {|h| -h.values.last }
+    end.sort_by {|k| -k.values.first[:duration] }
   end
 
   private
@@ -19,13 +22,13 @@ class Applications::SourcesController < Applications::ApplicationController
     to ||= Time.now.utc
 
     query = Druid::Query::Builder.new
-    query
-    .group_by([:scenario])
-    .granularity(:all)
-    .filter(application: event_source.application.id)
-    .filter(event_source: event_source.id)
-    .long_sum(:count)
-    .interval(from, to)
+    query.group_by([:scenario])
+      .granularity(:all)
+      .filter(application: event_source.application.id)
+      .filter(event_source: event_source.id)
+      .long_sum(:count)
+      .double_sum(:duration)
+      .interval(from, to)
 
     get(query)
   end
