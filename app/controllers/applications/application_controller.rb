8220bbc3a0e175ab5e9ab123fdc3b1905e33a63b
Andrew Kumanyaev <me@zzet.org>
2015-11-23 11:45:31 +0300

    Redirect to root if not authenticated

--- app/controllers/applications/application_controller.rb
+++ app/controllers/applications/application_controller.rb
@@ -1,10 +1,15 @@
 module Applications
   class ApplicationController < ::ApplicationController
+    before_action :authenticate!
 
     protected
 
     def application
       @application ||= current_user.applications.find_by(id: params[:application_id])
+    end
+
+    def authenticate!
+      redirect_to applications_path unless signed_in?
     end
   end
 end
