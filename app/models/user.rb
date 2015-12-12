992abb363c4c25ad54fb403e86248667369fff24
Artem Skripka <therewasadrizzle@gmail.com>
2015-12-13 01:50:03 +0700

    Add constant with availeble users, validation for inclusing role in that constant, method is_admin?

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,20 @@
 #
 
 class User < ActiveRecord::Base
+  USER_ROLES = ['User', 'Admin']
+
   has_many :application_users
   has_many :applications, through: :application_users
 
   before_create :generate_api_token
 
+  validates_inclusion_of :role, in: USER_ROLES
+
   def generate_api_token
     self[:api_token] = SecureRandom.hex(24)
+  end
+
+  def is_admin?
+    role == 'Admin'
   end
 end
